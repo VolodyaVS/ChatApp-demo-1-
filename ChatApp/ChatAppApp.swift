10d3ee4808d144058446228130ff658c6df0e5c4
Vladimir Stepanchikov <volodyavolga@protonmail.ch>
2021-11-22 19:19:46 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Vladimir Stepanchikov on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
