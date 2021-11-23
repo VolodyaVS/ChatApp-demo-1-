8a879200e02a2f66cadd24d690608512abe817ab
Vladimir Stepanchikov <volodyavolga@protonmail.ch>
2021-11-23 09:47:14 +0300

    App file was updated

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -6,14 +6,8 @@
 //
 
 import SwiftUI
-import Firebase
 
 struct ContentView: View {
-
-    init() {
-        FirebaseApp.configure()
-    }
-
     var body: some View {
         Text("Hello, world!")
             .padding()
